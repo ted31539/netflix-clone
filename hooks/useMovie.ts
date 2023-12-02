d6f3328852b3c389d6eede9c5c274dfccdf3197e
ted <ted31539@tappie.tw>
2023-12-02 23:21:29 +0800

    add movie page

---
+++ hooks/useMovie.ts
@@ -0,0 +1,17 @@
+import fetcher from '@/libs/fetcher';
+import useSWR from 'swr';
+
+const useMovie = (id?: string) => {
+  const { data, error, isLoading } = useSWR(id ? `/api/movies/${id}` : null, fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+  return {
+    data,
+    error,
+    isLoading
+  }
+};
+
+export default useMovie;
