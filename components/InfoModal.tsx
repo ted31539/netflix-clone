89e61f6299cc8440b01bcf79c3f0e9c684dcec0a
ted <ted31539@tappie.tw>
2023-12-03 21:59:12 +0800

    fix  infomodal ui bug

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -27,16 +27,29 @@ const InfoModal:React.FC<InfoModalProps> = ({visible, onClose})=> {
       setTimeout(()=>onClose(),300)
     }
 
+    useEffect(() => {
+      // this will disable the scroll if our back page was scrollable
+      if(visible) { 
+          document.body.classList.add("overflow-y-hidden");
+          document.body.style.top = `-${window.scrollY}px`;  
+      }
+      // when you close the modal, remove this class 
+      else {
+        document.body.classList.remove("overflow-y-hidden");
+        const scrollY = document.body.style.top;
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+      }
+    }, [visible]);
+
     if(!visible) return <></>
-
-
   
   return (
    <div className='z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0'>
-      <div className='relative w-auto mx-auto max-w-3xl rounded-md overflow-hidden'>
+      <div className='relative w-auto mx-w-screen max-w-3xl rounded-md overflow-x-hidden max-h-screen'>
         <div className={`${isVisible ? 'scale-100' : 'scale-0'} transform duration-300 relative flex-auto bg-zinc-900 drop-shadow-md `}>
 
-            <div className='relative h-96'>
+            <div className='relative h-96 max-h-screen'>
                 <video className='w-full brightness-[60%] object-cover h-full' autoPlay muted loop poster={data?.thumbnailUrl} src={data?.videoUrl} />
                 <div onClick={handleClose} className='cursor-pointer absolute top-3 right-3 h-10 w-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center'>
                     <AiOutlineClose className='text-white' size={20}/>
